78f8d2ac16b76b68dcf2bc3819a0569e92d9c447
NAHUEL <nahuelpierini@gmail.com>
2022-04-04 18:00:16 +0200

    ejercicio 2

---
+++ src/com/bootcamp/Main.java
@@ -0,0 +1,26 @@
+package com.bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numero1;
+
+        numero1 = 5;
+        System.out.println(numero1);
+
+        numero1 = 10;
+        System.out.println(numero1);
+
+        char letra1 = 'a';
+        char letra2 = 'b';
+        char letra3 = 'c';
+        char letra4 = 'd';
+
+
+        char[] letras = new char[]{letra1, letra2, letra3, letra4};
+        System.out.println(letras[0]);
+
+
+
+    }
+}
